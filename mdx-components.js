86c4a1492a410a9c0a3bac142becc18265670ad4
e-baron <raphael.baroni@vinci.be>
2024-04-06 15:11:34 +0200

    update: /app/slug/page simplified

--- mdx-components.js
+++ mdx-components.js
@@ -1,4 +1,175 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMoneyBill1Wave,
+  faMapLocation,
+  faCalendarCheck,
+  faEnvelope,
+  faPhone,
+  faCreditCard,
+  faList,
+  faBell,
+  faToolbox,
+} from "@fortawesome/free-solid-svg-icons";
+
 import Content from "src/components/content";
+import Image from "src/components/image";
+import Section from "src/components/section";
+import Footer from "src/components/footer";
+import SectionHeader from "src/components/section-header";
+import SectionMiddleTitle from "src/components/section-middle-title";
+import "src/scss/main.scss";
+import Scroll from "src/components/scroll/scroll";
+import Header from "src/components/header/header";
+import Carousel from "src/components/carousel/carousel.js";
+import NewsIndex from "src/components/news/news-index.js";
+import { format, parseISO } from "date-fns";
+import { allMDXPages } from "contentlayer/generated";
+import { useMDXComponent } from "next-contentlayer/hooks";
+
+import Link from "next/link";
+// import MainLayout from '../components/main-layout.js';
+import Background from "src/components/background.js";
+import SectionFooter from "src/components/section-footer.js";
+import PageHeader from "src/components/page-header.js";
+// The following import prevents a Font Awesome icon server-side rendering bug,
+// where the icons flash from a very large icon down to a properly sized one:
+import "@fortawesome/fontawesome-svg-core/styles.css";
+// Prevent fontawesome from adding its CSS since we did it manually above:
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false; 
+
+const siteURL = "https://www.artiyoga.com"; // No trailing slash allowed!
+const siteTitle = "artiYoga";
+const youtubeUrl = "https://www.youtube.com/channel/UCl_6cWf7A0yPr2GPW4uJ7lw"; //"https://www.youtube.com/channel/UC_iU0pfrDaYFXd6X9mPlAJQ";
+const authorEmail = "baroni.kati@gmail.com";
+const facebookUrl = "https://www.facebook.com/baroni.kati";
+const instagramUrl = "https://www.instagram.com/baroni.kati/";
+const defaultLanguage = "nl";
+const defaultAssociatedProjectGroupName = ""; // 'Web2 2023';
+const projectDocument = ""; //'WEB2-2022-PROJET-GROUP-XY.docx';
+
+const siteMetadata = {
+  version: "", // "2.0.0",
+  title: siteTitle,
+  description: "artiYoga : Yoga in 1500 Halle with Kati Baroni",
+  url: siteURL,
+  siteUrl: siteURL, // config for gatsby-plugin-robots-txt
+  youtubeUrl: youtubeUrl,
+  authorEmail: authorEmail,
+  facebookUrl: facebookUrl,
+  instagramUrl: instagramUrl,
+  languages: { langs: ["nl"], defaultLangKey: defaultLanguage },
+  defaultAssociatedProjectGroupName: defaultAssociatedProjectGroupName,
+  isAuthentication: true,
+  menuLinks: [
+    {
+      name: `Home`,
+      link: `/`,
+    },
+    {
+      name: "Lessen",
+      link: "",
+      subMenu: [
+        { name: "Groepslessen", link: "/lessons/groups" },
+        { name: "Yoga met de natuur", link: "/lessons/nature" },
+        { name: "VrouwenYoga Cirkel", link: "/lessons/women" },
+        { name: "Privé Yoga", link: "/lessons/personal" },
+        { name: "Online Yoga", link: "/lessons/video" },
+      ],
+    },
+    {
+      name: `About`,
+      link: ``,
+      subMenu: [
+        {
+          name: `Kati`,
+          link: `/about/kati`,
+        },
+        {
+          name: `Yogastijlen`,
+          link: `/about/yogastyles`,
+        },
+      ],
+    },
+    {
+      name: `Blog`,
+      link: `/news`,
+    },
+    {
+      name: `Coaching`,
+      link: `/coaching`,
+    },
+    {
+      name: `Contact`,
+      link: `/contact`,
+    },
+  ],
+};
+
+
+
+const MoneyIcon = ({ size = "2x" }) => (
+  <FontAwesomeIcon icon={faMoneyBill1Wave} fixedWidth size={size} />
+);
+
+const LocationIcon = ({ size = "2x" }) => (
+  <FontAwesomeIcon icon={faMapLocation} fixedWidth size={size} />
+);
+
+const CalendarIcon = ({ size = "2x" }) => (
+  <FontAwesomeIcon icon={faCalendarCheck} fixedWidth size={size} />
+);
+
+const EmailIcon = ({ size = "2x" }) => (
+  <FontAwesomeIcon icon={faEnvelope} fixedWidth size={size} />
+);
+
+const PhoneIcon = ({ size = "2x" }) => (
+  <FontAwesomeIcon icon={faPhone} fixedWidth size={size} />
+);
+
+const CreditCardIcon = ({ size = "2x" }) => (
+  <FontAwesomeIcon icon={faCreditCard} fixedWidth size={size} />
+);
+
+const ListIcon = ({ size = "2x" }) => (
+  <FontAwesomeIcon icon={faList} fixedWidth size={size} />
+);
+
+const BellIcon = ({ size = "2x" }) => (
+  <FontAwesomeIcon icon={faBell} fixedWidth size={size} />
+);
+
+const ToolIcon = ({ size = "2x" }) => (
+  <FontAwesomeIcon icon={faToolbox} fixedWidth size={size} />
+);
+
+const shortcodes = {
+  Link,
+  Image,
+  Section,
+  Content,
+  Carousel,
+  ContentWithBackground: Background,
+  Background,
+  SectionHeader,
+  SectionFooter,
+  SectionMiddleTitle,
+  PageHeader,
+  NewsIndex,
+  MoneyIcon,
+  LocationIcon,
+  CalendarIcon,
+  EmailIcon,
+  PhoneIcon,
+  CreditCardIcon,
+  ThemeIcon: ListIcon,
+  BellIcon,
+  ToolIcon,
+
+};
+
+/*import Content from "src/components/content";
 import Image from "src/components/image";
 import Section from "src/components/section";
 import Footer from "src/components/footer";
@@ -23,7 +194,7 @@ import PageHeader from "src/components/page-header.js";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 // Prevent fontawesome from adding its CSS since we did it manually above:
 import { config } from "@fortawesome/fontawesome-svg-core";
-config.autoAddCss = false; /* eslint-disable import/first */
+config.autoAddCss = false; 
 
 const siteURL = "https://www.artiyoga.com"; // No trailing slash allowed!
 const siteTitle = "artiYoga";
@@ -92,27 +263,6 @@ const siteMetadata = {
   ],
 };
 
-/* import { withFrontmatter } from './src/components/hoc/hoc.js';
-// import CodeBlock from './src/components/codeblock/codeblock.js';
-import LinkFile from './src/components/file/link-file.js';
-import ScrollableImage from './src/components/image/scrollable-image';
-import PublicProjectsView from './src/components/public-projects/public-projects-view';
-import {
-  AuthenticatedTemplate,
-  UnauthenticatedTemplate,
-} from '@azure/msal-react';
-import AuthenticatedBlock from './src/components/auth/authenticated-block.js';
-import UnAuthenticatedBlock from './src/components/auth/unauthenticated-block.js';
-import NestedMdxBlock from './src/components/mdx/nested-mdx-block.js';
-import YoutubeImage from './src/components/image/youtube-image.js';
-import InternalPageMenu from './src/components/internal-page-menu/internal-page-menu.js';
-import InternalPageMenuItem from './src/components/internal-page-menu/internal-page-menu-item.js';
-import InternalPageTitle from './src/components/internal-page-menu/menu-title.js';
-import {
-  PathViewer,
-  PathViewerItem,
-} from './src/components/path-viewer/path-viewer.js';
- */
 
 const shortcodes = {
   Link,
@@ -127,21 +277,12 @@ const shortcodes = {
   SectionMiddleTitle,
   PageHeader,
   NewsIndex,
-  /* CodeBlock,
-  LinkFile,
-  ScrollableImage,
-  PublicProjectsView,
-  AuthenticatedBlock,
-  UnAuthenticatedBlock,
-  NestedMdxBlock,
-  YoutubeImage,
-  InternalPageMenu,
-  InternalPageMenuItem,
-  InternalPageTitle,
-  PathViewer,
-  PathViewerItem,*/
-};
+};*/
 
+/* The generateStaticParams function can be used in combination with dynamic route 
+segments to statically generate routes at build time instead of on-demand at 
+request time.
+*/
 // Return a list of `params` to populate the [slug] dynamic segment
 export const generateStaticParams = async () =>
   allMDXPages.map((page) => ({ slug: page._raw.flattenedPath.split("/") }));
@@ -179,7 +320,7 @@ export const generateMetadata = ({ params }) => {
 // Multiple versions of this page will be statically generated
 // using the `params` returned by `generateStaticParams`
 export const MDXPageLayout = ({ params }) => {
-  // console.log("PostLayout() params: ", params);
+  console.log("MDXPageLayout() params: ", params);
   const page = allMDXPages.find(
     (mdxPage) => mdxPage._raw.flattenedPath === params.slug.join("/")
   );
